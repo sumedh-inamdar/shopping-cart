f855ed7ec065944a09761755b7099fbb2c03441f
Sumedh Inamdar <sinamdar@gmail.com>
2022-03-30 23:57:53 -0700

    Add animation + delay to homepage text

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,20 @@ export default function Home() {
     <div className="bg-gradient-to-r from-teal-500 to-indigo-500">
       <div className=" h-[720px]  max-w-6xl mx-auto">
         <div className="w-1/2 h-full">
-          <div className="flex flex-col h-full justify-center relative left-1/4 w-3/4 space-y-7">
+          <div className="flex flex-col h-full justify-center relative left-1/4 w-3/4 space-y-7 text-white">
             <div className="flex flex-col text-6xl space-y-3">
-              <div>GAME</div>
-              <div>SET</div>
-              <div>MATCH!</div>
+              <div className="animate-fade-in-down animation-delay-500">
+                GAME
+              </div>
+              <div className="animate-fade-in-down animation-delay-1000">
+                SET
+              </div>
+              <div className="animate-fade-in-down animation-delay-1500">
+                MATCH
+              </div>
             </div>
             <div className="text-xl">Lorem Ipsum</div>
-            <button className="bg-green-200 shadow-[6px_6px_2px_1px_rgba(0,0,255,0.4)]">
+            <button className="w-3/4 bg-gradient-to-r from-cyan-500 to-blue-500 shadow-[6px_6px_2px_1px_rgba(0,0,255,0.4)]">
               Shop Now
             </button>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,23 @@ module.exports = {
       },
       backgroundImage: {
         'tennis-court': "url('./assets/homepage-bg.jpg')"
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 1s ease-in'
       }
     }
   },
-  plugins: []
+  plugins: [require('tailwindcss-animation-delay')]
 };
